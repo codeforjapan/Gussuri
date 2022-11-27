346ba185fb06c03422759aaf788fb0afb60fcc14
kur00 <over.y31.bsk@gmail.com>
2022-11-27 21:48:28 +0900

    feat: added guide btn for aquarium page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:gussuri/aquarium.dart';
 import 'firebase_options.dart';
 import './home.dart';
 
@@ -39,7 +38,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Aquarium(),
+      body: Home(),
     );
   }
 }
