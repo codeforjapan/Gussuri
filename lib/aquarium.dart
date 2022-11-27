f30ad34c6baa6a181d231f080b420022448059c9
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2022-11-27 22:54:24 +0900

    feat: add whale

--- lib/aquarium.dart
+++ lib/aquarium.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gussuri/Whale.dart';
 
 class Aquarium extends StatefulWidget {
   const Aquarium({Key? key}) : super(key: key);
@@ -8,8 +9,9 @@ class Aquarium extends StatefulWidget {
 }
 
 class _AquariumState extends State<Aquarium>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late AnimationController controller;
+  List<Whale> whales = [];
 
   @override
   void initState() {
@@ -21,6 +23,9 @@ class _AquariumState extends State<Aquarium>
               controller.repeat();
             }
           });
+    Future.delayed(const Duration(seconds: 60), () {
+      addWhale();
+    });
     super.initState();
   }
 
@@ -33,34 +38,66 @@ class _AquariumState extends State<Aquarium>
   double shake(double value) =>
       1 * (0.5 - (0.5 - Curves.bounceOut.transform(value)).abs());
 
+  void addWhale() {
+    setState(() {
+      whales.add(Whale.create(
+          AnimationController(duration: const Duration(seconds: 2), vsync: this)
+            ..forward()
+            ..addListener(() {
+              if (whales.first.controller.isCompleted) {
+                setState(() {
+                  whales.first.dispose();
+                  whales.clear();
+                });
+              }
+            })));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
 
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-              decoration: const BoxDecoration(
-                color: Colors.black,
-                  image: DecorationImage(
-                      image: AssetImage('images/background.jpg'), fit: BoxFit.cover)),
-          ),
-          SafeArea(child:  AnimatedBuilder(
-            animation: controller,
-            builder: (context, child) => Transform.translate(
-              offset: Offset(200 * shake(controller.value), 0),
-              child: child,
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+              color: Colors.black,
+              image: DecorationImage(
+                  image: AssetImage('images/background.jpg'),
+                  fit: BoxFit.cover)),
+        ),
+        SafeArea(
+            child: Stack(
+          children: [
+            AnimatedBuilder(
+              animation: controller,
+              builder: (context, child) => Transform.translate(
+                offset: Offset(200 * shake(controller.value), 0),
+                child: child,
+              ),
+              child: const Image(
+                image: AssetImage('images/fish.gif'),
+                height: 100,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
             ),
-            child: const Image(
-              image: AssetImage('images/fish.gif'),
-              height: 100,
-              width: 100,
-              fit: BoxFit.cover,
-            ),
-          ))
-        ],
-      ),
+            ...whales.map((whale) => AnimatedBuilder(
+              animation: whale.controller,
+              builder: (context, child) => Transform.translate(
+                offset: Offset(200 * shake(whale.controller.value), 0),
+                child: child,
+              ),
+              child: const Image(
+                image: AssetImage('images/whale.png'),
+                height: 100,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+            ))
+          ],
+        ))
+      ],
     );
   }
 }
