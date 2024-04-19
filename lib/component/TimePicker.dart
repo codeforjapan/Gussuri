940da4ec3c8996306af33f2b243fe94e617c97a8
kur00 <over.y31.bsk@gmail.com>
2024-04-19 12:28:19 +0900

    fix: control timer picker state

--- lib/component/TimePicker.dart
+++ lib/component/TimePicker.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'dart:async';
 
 class TimePickerWidget extends StatefulWidget {
   final DateTime value;
@@ -11,34 +12,47 @@ class TimePickerWidget extends StatefulWidget {
 }
 
 class TimePickerState extends State<TimePickerWidget> {
-  dynamic dateTime;
-  late final ValueChanged<String?> submitOnChanged;
+
+  late DateTime dateTime;
+  Timer? _debounce;
 
   @override
   void initState() {
     super.initState();
-    if(widget.onChanged != null) {
-      submitOnChanged = widget.onChanged!;
-    }
     dateTime = widget.value;
+  }
+
+  void _onDateTimeChanged(DateTime dt) {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      setState(() {
+        dateTime = dt;
+        if (widget.onChanged != null) {
+          widget.onChanged!(dt.toString());
+        }
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: 100,
+        height: 150,
         width: 300,
         child: CupertinoDatePicker(
           key: UniqueKey(),
           backgroundColor: Colors.white,
           mode: CupertinoDatePickerMode.time,
           initialDateTime: dateTime,
-          onDateTimeChanged: (dt) {
-            setState(() => dateTime = dt);
-            submitOnChanged(dt.toString());
-          },
+          onDateTimeChanged: _onDateTimeChanged,
           use24hFormat: true,
           minuteInterval: 1,
         ));
+  }
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
   }
 }
