8d702db8507e475d8a7ec0bfc917398b81792bff
fukuchi <over.y31.bsk@gmail.com>
2025-01-31 04:01:28 +0900

    fix: order of evaluation score

--- lib/component/slide_button.dart
+++ lib/component/slide_button.dart
@@ -16,21 +16,21 @@ class SlideButton extends StatefulWidget {
 
 class SlideButtonState extends State<SlideButton>
     with TickerProviderStateMixin {
-  List<bool> isSelected = List.generate(11, (i) => false);
+  List<bool> isSelected = List.generate(11, (_) => false);
   late final Function(int?) submitOnChanged;
-
-  // todo ここを動的に動かしたい
-  final ScrollController _scrollController =
-      ScrollController(initialScrollOffset: 140.w);
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    if (widget.onChanged != null) {
-      submitOnChanged = widget.onChanged!;
-    }
-    setState(() {
-      isSelected[widget.value] = true;
+    submitOnChanged = widget.onChanged ?? (_) {};
+
+    // NOTE: 値を逆転
+    int reversedIndex = 10 - widget.value;
+    isSelected[reversedIndex] = true;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.jumpTo((reversedIndex * 40).w);
     });
   }
 
@@ -43,40 +43,42 @@ class SlideButtonState extends State<SlideButton>
       form: SizedBox(
         height: 70.h,
         child: ListView.builder(
-            shrinkWrap: true,
-            controller: _scrollController,
-            scrollDirection: Axis.horizontal,
-            itemCount: isSelected.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        isSelected = List.generate(11, (index) => false);
-                        isSelected[index] = true;
-                        submitOnChanged(index);
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        backgroundColor: Colors.white),
-                    child: ClipOval(
-                        child: Opacity(
+          shrinkWrap: true,
+          controller: _scrollController,
+          scrollDirection: Axis.horizontal,
+          itemCount: isSelected.length,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isSelected.fillRange(0, isSelected.length, false);
+                      isSelected[index] = true;
+                      submitOnChanged(10 - index); // NOTE:内部値を逆転
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      backgroundColor: Colors.white),
+                  child: ClipOval(
+                    child: Opacity(
                       opacity: isSelected[index] ? 1 : 0.5,
-                      child: Image.asset('images/evaluation_$index.jpg'),
-                    )),
+                      child: Image.asset('images/evaluation_${10 - index}.jpg'),
+                    ),
                   ),
-                  Text(
-                    '${localizations.rate} ${index + 1}',
-                    style: TextStyle(
-                        color: isSelected[index]
-                            ? Colors.black.withOpacity(1)
-                            : Colors.black.withOpacity(0.4)),
-                  )
-                ],
-              );
-            }),
+                ),
+                Text(
+                  '${localizations.rate} $index', // NOTE:ユーザーに見える値は変更しない
+                  style: TextStyle(
+                      color: isSelected[index]
+                          ? Colors.black
+                          : Colors.black.withOpacity(0.4)),
+                )
+              ],
+            );
+          },
+        ),
       ),
     );
   }
