023b473787a85d9b9c29d584e1a4fb610e5dd1ac
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2026-06-15 21:39:13 +0900

    feat: use NavigationRail on tablet, BottomNav on phone

--- lib/base.dart
+++ lib/base.dart
@@ -4,6 +4,7 @@ import 'package:gussuri/calendar.dart';
 import 'package:gussuri/component/header.dart';
 import 'package:gussuri/enums/TabItem.dart';
 import 'package:gussuri/home.dart';
+import 'package:gussuri/utils.dart';
 
 final _navigatorKeys = <TabItem, GlobalKey<NavigatorState>>{
   TabItem.home: GlobalKey<NavigatorState>(),
@@ -25,49 +26,80 @@ class Base extends HookWidget {
       currentTab.value = newTab;
     }
 
+    void onNavTap(int index) {
+      final selectedTab = TabItem.values[index];
+      if (currentTab.value == selectedTab) {
+        _navigatorKeys[selectedTab]
+            ?.currentState
+            ?.popUntil((route) => route.isFirst);
+      } else {
+        currentTab.value = selectedTab;
+        currentIndex.value = index;
+      }
+    }
+
+    final tabBody = Stack(
+      children: TabItem.values
+          .map(
+            (tabItem) => Offstage(
+              offstage: currentTab.value != tabItem,
+              child: Navigator(
+                key: _navigatorKeys[tabItem],
+                onGenerateRoute: (settings) {
+                  if (tabItem.page is Home) {
+                    return MaterialPageRoute<Widget>(
+                      builder: (context) =>
+                          Home(updateIndex: updateTab),
+                    );
+                  } else if (tabItem.page is Calendar) {
+                    return MaterialPageRoute<Widget>(
+                      builder: (context) =>
+                          Calendar(updateIndex: updateTab),
+                    );
+                  } else {
+                    return MaterialPageRoute<Widget>(
+                      builder: (context) => tabItem.page,
+                    );
+                  }
+                },
+              ),
+            ),
+          )
+          .toList(),
+    );
+
+    if (isTablet(context)) {
+      return Scaffold(
+        appBar: const Header(),
+        body: Row(
+          children: [
+            NavigationRail(
+              selectedIndex: currentIndex.value,
+              onDestinationSelected: onNavTap,
+              labelType: NavigationRailLabelType.selected,
+              indicatorColor: const Color(0xFFc5cae9),
+              destinations: TabItem.values
+                  .map(
+                    (tabItem) => NavigationRailDestination(
+                      icon: Icon(tabItem.icon),
+                      label: Text(tabItem.title(context)),
+                    ),
+                  )
+                  .toList(),
+            ),
+            const VerticalDivider(thickness: 1, width: 1),
+            Expanded(child: tabBody),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: const Header(),
-      body: Stack(
-        children: TabItem.values
-            .map(
-              (tabItem) => Offstage(
-                offstage: currentTab.value != tabItem,
-                child: Navigator(
-                  key: _navigatorKeys[tabItem],
-                  onGenerateRoute: (settings) {
-                    if(tabItem.page is Home) {
-                      return MaterialPageRoute<Widget>(
-                        builder: (context) => Home(updateIndex: updateTab),
-                      );
-                    } else if(tabItem.page is Calendar) {
-                      return MaterialPageRoute<Widget>(
-                        builder: (context) => Calendar(updateIndex: updateTab),
-                      );
-                    } else {
-                      return MaterialPageRoute<Widget>(
-                        builder: (context) => tabItem.page,
-                      );
-                    }
-                  },
-                ),
-              ),
-            )
-            .toList(),
-      ),
+      body: tabBody,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex.value,
-        onTap: (index) {
-          final selectedTab = TabItem.values[index];
-          if (currentTab.value == selectedTab) {
-            _navigatorKeys[selectedTab]
-                ?.currentState
-                ?.popUntil((route) => route.isFirst);
-          } else {
-            // 未選択
-            currentTab.value = selectedTab;
-            currentIndex.value = index;
-          }
-        },
+        onTap: onNavTap,
         items: TabItem.values
             .map(
               (tabItem) => BottomNavigationBarItem(
